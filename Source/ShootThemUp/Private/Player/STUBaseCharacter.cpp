0915daab2da661417e41ac7a7eb7bd9cc846f6a6
ValeraCherneikin <147158876+ValeraCherneikin@users.noreply.github.com>
2024-04-16 12:36:14 +0300

    Running animation

--- Source/ShootThemUp/Private/Player/STUBaseCharacter.cpp
+++ Source/ShootThemUp/Private/Player/STUBaseCharacter.cpp
@@ -5,8 +5,9 @@
 #include "Camera/CameraComponent.h"
 #include "Components/InputComponent.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "Components/STUCharacterMovementComponent.h"
 
-ASTUBaseCharacter::ASTUBaseCharacter()
+ASTUBaseCharacter::ASTUBaseCharacter(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer.SetDefaultSubobjectClass<USTUCharacterMovementComponent>(ACharacter::CharacterMovementComponentName))
 {
     PrimaryActorTick.bCanEverTick = true;
 
@@ -40,19 +41,37 @@ void ASTUBaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCo
     PlayerInputComponent->BindAxis("LookUp",this,&ASTUBaseCharacter::AddControllerPitchInput);
     PlayerInputComponent->BindAxis("TurnAround",this,&ASTUBaseCharacter::AddControllerYawInput);
     PlayerInputComponent->BindAction("Jump",IE_Pressed,this,&ASTUBaseCharacter::Jump);
+    PlayerInputComponent->BindAction("Run",IE_Pressed,this,&ASTUBaseCharacter::OnStartRunning);
+    PlayerInputComponent->BindAction("Run",IE_Released,this,&ASTUBaseCharacter::OnStopRunning);
     
     
 
 }
 
+bool ASTUBaseCharacter::bIsRunning() const
+{
+    return bWantsToRun && bIsMovingForward && !GetVelocity().IsZero();
+}
+
 void ASTUBaseCharacter::MoveForward(float Amount)
 {
+    bIsMovingForward = Amount > 0.0f;
     AddMovementInput(GetActorForwardVector(),Amount);
 }
 
 void ASTUBaseCharacter::MoveRight(float Amount)
 {
     AddMovementInput(GetActorRightVector(),Amount);
+}
+
+void ASTUBaseCharacter::OnStartRunning()
+{
+    bWantsToRun = true; 
+}
+
+void ASTUBaseCharacter::OnStopRunning()
+{
+    bWantsToRun = false;
 }
 
 
