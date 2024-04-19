4b647876948b8bb8d298d35f280dd40f64ecdfee
ValeraCherneikin <147158876+ValeraCherneikin@users.noreply.github.com>
2024-04-19 23:06:02 +0300

    ASpectatorPawn

--- Source/ShootThemUp/Private/Player/STUBaseCharacter.cpp
+++ Source/ShootThemUp/Private/Player/STUBaseCharacter.cpp
@@ -8,6 +8,7 @@
 #include "Components/STUCharacterMovementComponent.h"
 #include "Components/STUHealthComponent.h"
 #include "Components/TextRenderComponent.h"
+#include "GameFramework/Controller.h"
 #include "Engine/DamageEvents.h"
 
 DEFINE_LOG_CATEGORY_STATIC(LogBaseCharacter,All,All)
@@ -107,6 +108,10 @@ void ASTUBaseCharacter::OnDeath()
     GetCharacterMovement() -> DisableMovement();
 
     SetLifeSpan(5.0f);
+    if(Controller)
+    {
+        Controller->ChangeState(NAME_Spectating);
+    }
 }
 
 void ASTUBaseCharacter::OnHealthChanged(float Health)
