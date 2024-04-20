aa50da0196fb92360fb1f71173e7e071adb4de7e
ValeraCherneikin <147158876+ValeraCherneikin@users.noreply.github.com>
2024-04-20 13:19:37 +0300

    Landing Damage

--- Source/ShootThemUp/Public/Player/STUBaseCharacter.h
+++ Source/ShootThemUp/Public/Player/STUBaseCharacter.h
@@ -36,6 +36,11 @@ protected:
     UPROPERTY(EditDefaultsOnly,Category = "Animation")
     UAnimMontage* DeathAnimMontage;
 
+    UPROPERTY(EditDefaultsOnly,Category = "MoveMent")
+    FVector2D LandedDamageVelocity = FVector2D(900.0f,1200.0f);
+    UPROPERTY(EditDefaultsOnly,Category = "MoveMent")
+    FVector2D LandedDamage = FVector2D(10.0f,100.0f);
+
     virtual void BeginPlay() override;
     
 
@@ -60,4 +65,7 @@ private:
 
     void OnDeath();
     void OnHealthChanged(float Health);
+
+    UFUNCTION()
+    void OnGroundLanded(const FHitResult& Hit);
 };
